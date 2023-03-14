547ac3af9ccfafcc5250a171e66d620ec7068460
EdwinprasW <weather2402@gmail.com>
2023-03-14 21:35:07 +0700

    Javascript Day 2

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
 const book = {
     "name": "Laskar Pelangi",
     "price": 57500,
-    "inStock": true
+    "inStock": true,
+    "stock": 15,
+    "order": 12
 }
 
 Purchase(book, 30, 10);
 
+function Purchase({ name, price, inStock, stock, order }, dc, tax) {
 
-function Purchase({ name, price, inStock }, dc, tax) {
+
+    let bookBuyed = 0;
+    let totalPrice = 0;
     const discount = price * (dc / 100);
     const totalDiscount = price - discount;
     const theTax = totalDiscount * (tax / 100);
     const taxTotal = totalDiscount + theTax;
+    let i = stock;
+    let n = order;
+
+    while (i > 0) {
+        totalPrice = taxTotal + totalPrice;
+        bookBuyed = bookBuyed + 1;
+        i = i - 1;
+        n = n - 1;
+
+        if (i == 0 || n == 0) {
+            break;
+        }
+    }
 
     console.log('Judul          : ', name);
-    console.log('Harga          : ', price);
+    console.log('Harga          :  Rp.', price);
     if (inStock === true) {
         console.log('In Stock       :  Yes');
     } else {
         console.log('In Stock       :  No');
     }
-    console.log('Diskon         : ', discount);
-    console.log('Setelah Diskon : ', totalDiscount);
-    console.log('Pajak          : ', theTax);
-    console.log('Total          : ', taxTotal);
+    console.log('Buku Terbeli   : ', bookBuyed, ' pcs');
+    console.log('Diskon         :  Rp.', discount);
+    console.log('Setelah Diskon :  Rp.', totalDiscount);
+    console.log('Pajak          :  Rp.', theTax);
+    console.log('Harga per Buku :  Rp.', taxTotal);
+    console.log('Harga Total    :  Rp.', totalPrice);
 
+    if (i == 0 && n > 0) {
+        console.log('that book is already out of stock!');
+        console.log('sisa buku tidak terbeli adalah ', n, ' pcs');
+
+    } else {
+        console.log('semua pesanan terbeli :D!');
+        console.log('sisa stock buku adalah ', i, ' pcs');
+    }
 
     return 0;
 }
